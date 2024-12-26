1882d21d72754e3185fe9192a8f4aeb024171dab
Mokhtar Khamam <mokhtar.khamam123@gmail.com>
2024-12-27 00:32:02 +0300

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,38 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "test",
+//         port: "",
+//         pathname: "/images/**",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
+// module.exports = {
+//   async headers() {
+//     return [
+//       {
+//         // matching all API routes
+//         source: "/api/:path*",
+//         headers: [
+//           { key: "Access-Control-Allow-Credentials", value: "true" },
+//           { key: "Access-Control-Allow-Origin", value: "*" },
+//           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+//           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+//         ]
+//       }
+//     ]
+//   }
+// };
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -5,16 +40,10 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "test",
-        port: "",
         pathname: "/images/**",
       },
     ],
   },
-};
-
-export default nextConfig;
-
-module.exports = {
   async headers() {
     return [
       {
@@ -25,9 +54,10 @@ module.exports = {
           { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
+        ],
+      },
+    ];
+  },
 };
 
+export default nextConfig;
